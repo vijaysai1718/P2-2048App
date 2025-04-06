1954b07ba7ac1ed23ed84a0e6266dabce797f17d
vijaysai <vijaysai.chinthala45@gmail.com>
2025-04-06 10:11:02 +0530

    installation file changes

--- Terraform/Installation.sh
+++ Terraform/Installation.sh
@@ -60,23 +60,14 @@ validate $? "Aws CLI"
 
 
  #Installation of jenkins 
-#!/bin/bash
- apt update -y
-#sudo apt upgrade -y
-wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc
-echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
- apt update -y
- apt install temurin-17-jdk -y
-/usr/bin/java --version
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | ] tee \
-                  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-                  https://pkg.jenkins.io/debian-stable binary/ |  tee \
-                              /etc/apt/sources.list.d/jenkins.list > /dev/null
- apt-get update -y
- apt-get install jenkins -y
+ apt update &&  apt upgrade -y
+ apt install openjdk-17-jdk -y
+ curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]  https://pkg.jenkins.io/debian binary/ |  tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+ apt update
+ apt install jenkins -y
  systemctl start jenkins
- systemctl status jenkins
+ systemctl enable jenkins
 
  #Installation of trivy 
  apt-get install wget apt-transport-https gnupg lsb-release -y
