db9db3cc55eac1b56b588b0ca8620740d494a497
vijaysai <vijaysai.chinthala45@gmail.com>
2025-04-07 12:23:01 +0530

    new

--- Terraform/Installation.sh
+++ Terraform/Installation.sh
@@ -78,3 +78,10 @@ echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.
  apt-get update
  apt-get install trivy -y
 
+
+# eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+mv /tmp/eksctl /usr/local/bin
+eksctl version
+VALIDATE $? "eksctl installation"
+
